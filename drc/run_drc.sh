15e0440a214162a974975a3206eac035da4bf801
azwefabless <andrew.wright@efabless.com>
2024-06-12 18:39:04 -0400

    aDDING AUTOMATION FOR drc TOAL
    Signed-off-by: azwefabless <andrew.wright@efabless.com>

--- drc/run_drc.sh
+++ drc/run_drc.sh
@@ -1,17 +1,17 @@
 #! /bin/bash
 magic -dnull -noconsole -rcfile /usr/share/pdk/sky130A/libs.tech/magic/sky130A.magicrc << EOF
-load ../mag/sky130_hcmus_ip__instramp
+load ../mag/IA_v4
 select top cell
 expand
 drc style drc(full)
 drc on
 drc check
 drc catchup
-drc count
+drc count total
 set tk_version 8.5
 source \${CAD_ROOT}/magic/tcl/drcmgr.tcl
 #feedback save drc.log
-magic::drc_save_report sky130_hcmus_ip__instramp drc.log
+magic::drc_save_report IA_v4 drc.log
 EOF
 exit 0
 
