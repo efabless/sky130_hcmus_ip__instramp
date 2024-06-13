16cb85cf6952594712ff8a875ee66276ed4adf7c
azwefabless <andrew.wright@efabless.com>
2024-06-13 10:53:38 -0400

    adding commit for lvs and drc uatomation
    Signed-off-by: azwefabless <andrew.wright@efabless.com>

--- mag/run_extract_lay.sh
+++ mag/run_extract_lay.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
 magic -dnull -noconsole -rcfile \$PDK_ROOT/\$PDK/libs.tech/magic/sky130A.magicrc << EOF
-load sky130_hcmus_ip__instramp
+load IA_v4
 select top cell
 extract path extfiles
 extract all
 ext2spice lvs
-ext2spice -p extfiles -o ../netlist/layout/sky130_hcmus_ip__instramp.spice
+ext2spice -p extfiles -o ../netlist/layout/IA_v4.spice
 quit -noprompt
 EOF
 rm -r extfiles
